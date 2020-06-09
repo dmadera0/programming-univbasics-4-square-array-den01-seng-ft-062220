2dbe5705c93e9c3ae5705f8d3fda5d1011438537
Daniel Madera <dmadera0@gmail.com>
2020-06-09 17:50:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(numbers)
   new_numbers=[]
    numbers.each do |numbers|
-    new_numbers << numbers**2 
+     number = numbers**2
+    new_numbers
   end
   return new_numbers
 end
